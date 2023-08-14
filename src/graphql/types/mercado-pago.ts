b6a745a10fcb51a7aaf26e846c34f2757f2dcd65
Facundo Dobboletta <facudobbo@gmail.com>
2023-08-14 19:26:10 -0300

    feat: Ability to get and save mercado pago access token for a given user

--- src/graphql/types/mercado-pago.ts
+++ src/graphql/types/mercado-pago.ts
@@ -6,7 +6,10 @@ import {
   queryField,
   stringArg,
 } from 'nexus';
-import { getMercadoPagoPreferenceId } from '../services/mercado-pago';
+import {
+  getMercadoPagoPreferenceId,
+  getMercadoPagoAccessToken,
+} from '../services/mercado-pago';
 
 export const MercadoPagoPreferenceObject = objectType({
   name: 'MercadoPagoPreference',
@@ -35,29 +38,8 @@ export const getLastMercadoPagoPreference = queryField(
 export const authorizeMercadoPago = mutationField('authorizeMercadoPago', {
   type: MercadoPagoAccessToken,
   args: {
-    userId: nonNull(stringArg()),
     mercadoPagoCode: nonNull(stringArg()),
   },
-  resolve: async (_, args) => {
-    try {
-      const redirectUri = 'https://comuniprode.netlify.app/admin/integrations';
-      const response = await axios.post(
-        'https://api.mercadopago.com/oauth/token',
-        {
-          grant_type: 'authorization_code',
-          client_id: process.env.MERCADO_PAGO_CLIENT_ID,
-          client_secret: process.env.MERCADO_PAGO_CLIENT_SECRET,
-          code: args.mercadoPagoCode,
-          redirect_uri: redirectUri, // Prueba
-        },
-      );
-
-      console.log('respuesta API mercadoPago ', response.data);
-
-      return { accessToken: '' };
-    } catch (error) {
-      console.error(error);
-      throw new Error('Error al obtener el token de acceso');
-    }
-  },
+  resolve: async (_, args, ctx) =>
+    await getMercadoPagoAccessToken(args.mercadoPagoCode, ctx.userId || ''),
 });
