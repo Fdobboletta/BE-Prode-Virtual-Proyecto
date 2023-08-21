82ea7020e1de048c4ef3d6e371c7debaa80a5608
Facundo Dobboletta <facudobbo@gmail.com>
2023-08-21 14:37:57 -0300

    fix: notification url test

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const server = new ApolloServer({
 
 // New endpoint for Mercado Pago notifications
 app.post('/mercado-pago-notification', (req, res) => {
-  console.log('MERCADO PAGOOO DATA: ', res.body);
+  console.log('MERCADO PAGOOO DATA: ', req);
   res.sendStatus(200);
 });
 
