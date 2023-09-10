6d3cc51a91c65610703160386626c3a4f7c195ec
Facundo Dobboletta <facudobbo@gmail.com>
2023-09-10 00:02:10 -0300

    fix: Power bi access Token endpoint WORKS

--- src/graphql/types/power-bi.ts
+++ src/graphql/types/power-bi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 import { nonNull, queryField } from 'nexus';
 
 export const getPowerBiAccessToken = queryField('getPowerBiAccessToken', {
@@ -6,20 +7,17 @@ export const getPowerBiAccessToken = queryField('getPowerBiAccessToken', {
   resolve: async (_, args, ctx) => {
     try {
       const params = new URLSearchParams();
-      params.append('client_id', 'af3cfb14-a704-4f7b-8a45-61df2d20ca3c');
-      params.append(
-        'client_secret',
-        'UmQ8Q~TJHwV0MUqmY1bx6v3JzaVWijt16TQ8Cbpf',
-      );
-      params.append('grant_type', 'client_credentials');
-      params.append(
-        'scope',
-        'https://analysis.windows.net/powerbi/api/.default',
-      );
 
+      params.append('client_id', process.env.POWER_BI_CLIENT_ID || '');
+      params.append('client_secret', process.env.POWER_BI_CLIENT_SECRET || '');
+      params.append('grant_type', 'password');
+      params.append('resource', 'https://analysis.windows.net/powerbi/api');
+      params.append('scope', 'openid');
+      params.append('username', process.env.POWER_BI_USER || '');
+      params.append('password', process.env.POWER_BI_PASS || '');
       const response = await axios.post(
-        'https://login.microsoftonline.com/2dd23f82-d7a2-4f1a-85ff-622ebf7d878c/oauth2/v2.0/token',
-        params.toString(),
+        `https://login.microsoftonline.com/${process.env.POWER_BI_APP_ID}/oauth2/token`,
+        params,
         {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -29,7 +27,8 @@ export const getPowerBiAccessToken = queryField('getPowerBiAccessToken', {
 
       return response.data.access_token;
     } catch (error: any) {
-      throw new Error(`${error.message}`);
+      console.log('error', error);
+      throw new Error(`Error al obtener el token: ${error.message}`);
     }
   },
 });
